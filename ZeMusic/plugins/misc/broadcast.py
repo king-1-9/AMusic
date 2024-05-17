7e2c69c35aef2643b450a7e00cf9c04a7b27d5da
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-17 23:48:35 +0300

    Update broadcast.py

--- ZeMusic/plugins/misc/broadcast.py
+++ ZeMusic/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command(["‍اذاٌٌع‍‌ةةِ", "اذِاٌٍع‍‌ةة‍‍"]) & SUDOERS)
+@app.on_message(filters.command(["اذاعه", "/broadcast"],"") & OWNER_ID)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
