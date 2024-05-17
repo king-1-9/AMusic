42b105fc55b9052c307dd8513ef0cecf5260b0a8
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-17 20:22:53 +0300

    Update deev.py

--- ZeMusic/plugins/Managed/deev.py
+++ ZeMusic/plugins/Managed/deev.py
@@ -18,7 +18,7 @@ async def huhh(client: Client, message: Message):
     dev = await client.get_users(OWNER_ID)
     name = dev.first_name
     
-    await message.reply_photo(
+    await message.reply_text(
         caption=f"""<b>Dev ↠ <a href='tg://user?id={OWNER_ID}'>{name}</a></b>""",
         reply_markup=InlineKeyboardMarkup(
             [
