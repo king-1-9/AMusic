8e0868e3d915a303f278337d10e58b23d0648613
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-18 10:19:59 +0300

    Update deev.py

--- ZeMusic/plugins/Managed/deev.py
+++ ZeMusic/plugins/Managed/deev.py
@@ -3,22 +3,17 @@ import os
 import time
 import requests
 from config import START_IMG_URL, OWNER_ID
-from pyrogram import filters
-import random
 from pyrogram import Client, filters, emoji
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from strings.filters import command
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import app
-from random import choice, randint
 
-@app.on_message(filters.regex(r"^(.)$"))
+@app.on_message(filters.text & filters.regex(r"^\.$"))
 async def huhh(client: Client, message: Message):
     dev = await client.get_users(OWNER_ID)
     name = dev.first_name
 
     await message.reply(
-        text=f"""<b>Dev ↠ <a href='tg://user?id={OWNER_ID}'>{name}</a></b>""",
+        text=f"""<b>Dev ↠ <a href="tg://user?id={OWNER_ID}">{name}</a></b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -27,5 +22,8 @@ async def huhh(client: Client, message: Message):
                 ],
             ]
         ),
-        reply_to_message_id=message.id  # This ensures the bot replies to the user's message
+        reply_to_message_id=message.id  # This ensures the bot replies to the user s message
     )
+
+if __name__ == "__main__":
+    app.run()
