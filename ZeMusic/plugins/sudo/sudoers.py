c7a800449fb5bb7c1cb06d98b112c4a3dd18426e
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-24 01:48:05 +0300

    Update sudoers.py

--- ZeMusic/plugins/sudo/sudoers.py
+++ ZeMusic/plugins/sudo/sudoers.py
@@ -7,7 +7,7 @@ from ZeMusic.utils.database import add_sudo, remove_sudo
 from ZeMusic.utils.decorators.language import language
 from ZeMusic.utils.extraction import extract_user
 from ZeMusic.utils.inline import close_markup
-from config import DAV, BANNED_USERS, OWNER_ID
+from config import BANNED_USERS, OWNER_ID
 
 
 @app.on_message(filters.command(["رفع مطور"],"") & filters.user(OWNER_ID))
@@ -54,8 +54,8 @@ async def userdel(client, message: Message, _):
         if "@" in user:
             user = user.replace("@", "")
         user = await app.get_users(user)
-        if DAV == user.id:
-            return await message.reply_text(_["abod"].format(user.mention))
+        #if DAV == user.id:
+            #return await message.reply_text(_["abod"].format(user.mention))
         if user.id not in SUDOERS:
             return await message.reply_text(_["sudo_3"].format(user.mention))
         removed = await remove_sudo(user.id) 
@@ -66,8 +66,8 @@ async def userdel(client, message: Message, _):
         await message.reply_text(f"حدث خطاء.")
         return
     user = await extract_user(message)
-    if DAV == user.id:
-        return await message.reply_text(_["abod"].format(user.mention))
+    #if DAV == user.id:
+        #return await message.reply_text(_["abod"].format(user.mention))
     if user.id not in SUDOERS:
         return await message.reply(_["sudo_3"].format(user.mention))
     removed = await remove_sudo(user.id)
